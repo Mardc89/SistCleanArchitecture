f2ac8af2c95411825b5b5a54811ba84990d38a05
Mardc89 <mardc0602@gmail.com>
2025-12-14 13:08:52 -0500

    Agregando DomainErrors

---
+++ CleanArchitecture3D/Domain/DomainErrors/DomainErrors.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DomainErrors
+{
+    public static partial class Errors
+    {
+        public static class Customer
+        {
+            public static Error PhoneNumberWidthBadFormat =>
+                Error.Validation("customer.PhoneNumber","Phone number has not valid format.");
+
+            public static Error AddressNumberWidthBadFormat =>
+                Error.Validation("customer.Adress", "Adress is not valid");
+        }
+
+    }
+}
--- CleanArchitecture3D/TestUnit/CreateCustomerCommandHandlerTest.cs
+++ CleanArchitecture3D/TestUnit/CreateCustomerCommandHandlerTest.cs
@@ -1,8 +1,10 @@
 using Application.Customers.Create;
 using Domain.Customers;
 using Domain.Primitives;
+using FluentAssertions;
 using Moq;
-
+using System.Threading.Tasks;
+using Domain.DomainErrors;
 namespace TestUnit
 {
     public class CreateCustomerCommandHandlerTest
@@ -22,9 +24,29 @@ namespace TestUnit
                 
         }
         [Fact]
-        public void HandleCreateCustomer_WhenPhoneNumberHasBadFormat_ShouldReturnValidationError()
+        public async Task HandleCreateCustomer_WhenPhoneNumberHasBadFormat_ShouldReturnValidationError()
         {
+          CreateCustomerCommand command=new CreateCustomerCommand(
+              "Lobert",
+              "Lopez",
+              "lopez34gmail.com",
+              "3232434434",
+              "",
+              "",
+              "",
+              "",
+              "",
+              ""
+              
+              
+          );
 
+            var result = await _handler.HandleAsync(command, default);
+
+            result.IsError.Should().BeTrue();
+            result.FirstError.Type.Should().Be(ErrorOr.ErrorType.Validation);
+            result.FirstError.Code.Should().Be(Errors.Customer.PhoneNumberWidthBadFormat.Code);
+            result.FirstError.Description.Should().Be(Errors.Customer.PhoneNumberWidthBadFormat.Description);
         }
     }
 }
